86c2313f7127ad82aa0d6cdd00e714c211c73414
Jonathan <jon.bluks@gmail.com>
2019-08-29 10:31:57 -0700

    Added /

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
 
+app.get('/', (req, res) => {
+  res.send('This api generates a random number at /random')
+})
+
 app.get('/random', (req, res) => {
   res.json({ number: Math.floor(Math.random()*1000)+1})
 })
